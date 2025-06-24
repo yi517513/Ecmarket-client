c63d11e91910c1b37ed8c3532a0879e9b3a458f5
YI <yi517513@gmail.com>
2025-06-24 17:12:59 +0800

    update

--- src/features/image/components/DeleteImageButton.jsx
+++ src/features/image/components/DeleteImageButton.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { useDeleteImage } from "../hooks/useImageMutate";
+import { useConfirmDialogStore } from "@stores/useConfirmDialogStore";
 
 export const DeleteImageButton = ({ _id }) => {
+  const openConfirm = useConfirmDialogStore((s) => s.openConfirm);
   const { mutate: deleteImage } = useDeleteImage();
+
+  const handleDeleteImg = useCallback(async () => {
+    const confirmed = await openConfirm("是否刪除圖片? 此操作無法還原");
+    if (!confirmed) return;
+
+    deleteImage(_id);
+  }, [_id]);
 
   return (
     <>
-      <button className="btn-danger mx-auto" onClick={() => deleteImage(_id)}>
+      <button className="btn-danger mx-auto" onClick={handleDeleteImg}>
         刪除
       </button>
     </>
